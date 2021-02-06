81e583168fb7d18ddc5c2b749d5294cd67f8fe95
Jack <jacka73@verizon.net>
2021-02-06 15:55:49 -0500

    Starting Website

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/', methods=['GET'])
+def home():
+    
+    if request.method == 'GET':
+        pass
+
+
+    return render_template('home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
