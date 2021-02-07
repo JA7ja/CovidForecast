5cfbc48ef2280b02338bc47abf0728d9f7a0aa2b
Jack Albertson <36114932+JA7ja@users.noreply.github.com>
2021-02-07 00:22:02 -0500

    Backend api (#5)
    * First commit on the branch
    
    * added county
    
    * added historicalapi
    
    * edited models.py

--- covidapi.py
+++ covidapi.py
@@ -1,35 +1,65 @@
 import requests
 class Covid():
     def __init__(self, type, name):
-        self.type = type
-        if self.type == "country":
-            self.state = "countries/"
-        if self.type == "state":
-            self.state = "states/"
-        self.base_url = "https://corona.lmao.ninja/v2/"
-        self.name_of_place = str(name)
-        self.confirmed_cases = 0
-        self.today_cases = 0
-        self.today_deaths = 0
-        self.recovered = 0
-        self.active_cases = 0
-        self.deaths = 0
-        self.life_expectancy = 0
-        self.get()
+        try:
+            self.type = type.lower()
+            if self.type == "country":
+                self.state = "countries/"
+            elif self.type == "state":
+                self.state = "states/"
+            elif self.type == "county":
+                self.state = "jhucsse/counties/"
+            self.base_url = "https://corona.lmao.ninja/v2/"
+            self.name_of_place = str(name)
+            self.confirmed_cases = 0
+            self.today_cases = 0
+            self.today_deaths = 0
+            self.recovered = 0
+            self.active_cases = 0
+            self.deaths = 0
+            self.life_expectancy = 0
+            self.get()
+        except AttributeError:
+            print("Your Country, County, or State does not exist!")
     def get(self):
-        self.requesturl = self.base_url + self.state + self.name_of_place
-        self.request = requests.get(self.requesturl)
-        self.response = self.request.json()
-        self.confirmed_cases = self.response["cases"]
-        self.today_cases = self.response["todayCases"]
-        self.today_deaths = self.response["todayDeaths"]
-        self.recovered = self.response["recovered"]
-        self.active_cases = self.response["active"]
-        self.deaths = self.response["deaths"]
-        self.deaths_per_million = self.response["deathsPerOneMillion"]
+        try:
+            self.requesturl = self.base_url + self.state + self.name_of_place
+            self.request = requests.get(self.requesturl)
+            self.response = self.request.json()
+            if self.type == "county":
+                self.confirmed_cases = self.response[0]["stats"]["confirmed"]
+                self.deaths = self.response[0]["stats"]["deaths"]
+                self.recovered = self.response[0]["stats"]["recovered"]
+            elif self.type == "state" or "country":
+                self.confirmed_cases = self.response["cases"]
+                self.today_cases = self.response["todayCases"]
+                self.today_deaths = self.response["todayDeaths"]
+                self.recovered = self.response["recovered"]
+                self.active_cases = self.response["active"]
+                self.deaths = self.response["deaths"]
+                self.deaths_per_million = self.response["deathsPerOneMillion"]
+        except KeyError:
+            print("Your Country, County, or State does not exist!")
     def __str__(self):
-        string = "Confirm cases : " + str(self.confirmed_cases) + " Today's cases : " + str(self.today_cases) + " Today's Deaths : " + str(self.today_deaths) + " Number of recovered: " + str(self.recovered) + " Active cases : " + str(self.active_cases) + " Number Of deaths: " + str(self.deaths) + " Deaths Per Million : " + str(self.deaths_per_million)
-        return string
+            try:
+                if self.type == "county":
+                    confirm_cases = "Confirm cases : " + str(self.confirmed_cases)
+                    recovered = " Number of recovered: " + str(self.recovered)
+                    deaths = " Number Of deaths: " + str(self.deaths)
+                    string = confirm_cases + deaths + recovered
+                elif self.type == "country" or "state":
+                    confirm_cases = "Confirm cases : " + str(self.confirmed_cases)
+                    today_cases = " Today's cases : " + str(self.today_cases)
+                    today_death = " Today's Deaths : " + str(self.today_deaths)
+                    recovered = " Number of recovered: " + str(self.recovered)
+                    active_cases =  " Active cases : " + str(self.active_cases)
+                    deaths = " Number Of deaths: " + str(self.deaths)
+                    deaths_per_million = " Deaths Per Million : " + str(self.deaths_per_million)
+                    string = confirm_cases + today_cases + today_death + recovered + active_cases + deaths + deaths_per_million
+                return string
+            except:
+                string = "Your Country, County, or State does not exist!"
+                return string
 
 covid = Covid("state", "New York")
 print(covid)
--- models.py
+++ models.py
@@ -1,3 +1,26 @@
 from os import path
-
+import covidapi
+import historicalapi
 ROOT = path.dirname(path.relpath((__file__)))
+
+def confirmCases(type, name):
+    detect_case = Covid(type, name)
+    return detect_case.confirmed_cases
+def deaths(type, name):
+    detect_case = Covid(type, name)
+    return detect_case.deaths
+def recovered(type, name):
+    detect_case = Covid(type, name)
+    return detect_case.recovered
+def todayCases(type, name):
+    detect_case = Covid(type, name)
+    return detect_case.today_cases
+def todayDeaths(type, name):
+    detect_case = Covid(type, name)
+    return detect_case.today_deaths
+def deaths_per_million(type, name):
+    detect_case = Covid(type, name)
+    return detect_case.deaths_per_million
+def historicalapi(type, name, days):
+    detect_case = Historical(type, name, days)
+    return detect_case
